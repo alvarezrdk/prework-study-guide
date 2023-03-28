a57073e98612a675f3ffbe63135d96764ae7da1b
Ruben <alvarezrdk@gmail.com>
2023-03-28 16:49:17 -0400

    Added JS to Project

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+}
+
+/*
+var studentInfo = ["Lu", 54, true];
+console.log(studentInfo);
+
+var shapes = ["triangle", "square", "pentagon", "circle"];
+
+for(var x = 3; x > 0; x--) {
+    console.log(shapes[x]);
+   }
+
+*/
